561a19b9f6ad18b4c3654614d89d34259e177f88
AdamsAppios <soadam390@gmail.com>
2023-08-17 11:35:40 +0800

    Commited

--- src/Components/Hompepage.js
+++ src/Components/Hompepage.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sendreport from "./Moonlitreports/Sendreport";
 import Moneymain from "./MoneyCounter/Moneymain";
 import GlorybeeMain from './Glorybee/GlorybeeMain';
+import RefillingCCTVMain from "./RefillingCCTV/RefillingCCTVMain";
 function Homepage() {
   return (
     <Router>
@@ -12,7 +13,7 @@ function Homepage() {
         <Route path="/sendreport" element={<Sendreport />} />
         <Route path="/moneycount" element={<Moneymain />} />
         <Route path="/glorybee" element={<GlorybeeMain />} />
-
+        <Route path="/refcccount" element={<RefillingCCTVMain />} />
       </Routes>
     </Router>
   );
